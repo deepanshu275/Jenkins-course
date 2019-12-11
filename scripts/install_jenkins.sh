9673d5acfe326972af0d7908d908e4be314ff54f
deepanshu275 <53260871+deepanshu275@users.noreply.github.com>
2019-12-11 15:07:20 +0530

    Update install_jenkins.sh

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -14,7 +14,15 @@ usermod -aG docker ubuntu
 # run jenkins
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
-docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+docker run \
+  -u root \
+  --rm \
+  -d \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  jenkinsci/blueocean
 
 # show endpoint
 echo 'Jenkins installed'
